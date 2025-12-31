05fef7ad3bae0007e3562cb50bfac2a783cb14d4
beskU <149000826+beskU@users.noreply.github.com>
2025-12-31 21:15:14 +0100

    Use variable for base URL in sitemap
    Replaced hardcoded base URL with a variable for consistency.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,32 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://rrota.xyz";
+
   return [
     {
-      url: "https://rrota.xyz",
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
+    },
+    {
+      url: `${baseUrl}/#Tokenomics`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/#HowToBuy`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/#Roadmap`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
     },
   ];
 }
