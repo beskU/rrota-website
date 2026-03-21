533ab3c4030ecd6296397f617183469b30daf996
beskU <149000826+beskU@users.noreply.github.com>
2026-03-21 23:28:30 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
-// tailwind.config.js
-const {heroui} = require("@heroui/react");
+const { heroui } = require("@heroui/react");
+const typography = require("@tailwindcss/typography");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [heroui()],
+  plugins: [heroui(), typography],
 };
