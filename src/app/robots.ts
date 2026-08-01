799444f403cd12aff47f46803f25a4a3b3fd3193
beskU <149000826+beskU@users.noreply.github.com>
2026-08-01 18:03:53 +0200

    Refactor robots function to use SITE_URL constant

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,17 +1,15 @@
 import type { MetadataRoute } from "next";
 
-const BASE_URL = "https://rrota.xyz";
+const SITE_URL = "https://rrota.xyz";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/api/", "/admin/", "/private/", "/draft/"],
-      },
-    ],
-    sitemap: `${BASE_URL}/sitemap.xml`,
-    host: BASE_URL,
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api", "/admin", "/private", "/draft"],
+    },
+    sitemap: `${SITE_URL}/sitemap.xml`,
+    host: SITE_URL,
   };
 }
